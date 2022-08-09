bff92dcb629690271a85c93fa3579810ef8a3798
wooooozin <wooooozin@gmail.com>
2022-08-09 19:18:02 +0900

    [feat] Assets 추가 및 뷰 설정

--- I AM RICH Swift UI/I AM RICH Swift UI/ContentView.swift
+++ I AM RICH Swift UI/I AM RICH Swift UI/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.orange
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I AM RICH")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 8")
     }
 }

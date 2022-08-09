5744fc7e570844896bc0ed09ac062a2d3a2fe2f4
wooooozin <wooooozin@gmail.com>
2022-08-09 18:46:56 +0900

    [feat] swift ui

---
+++ I AM RICH Swift UI/I AM RICH Swift UI/I_AM_RICH_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_AM_RICH_Swift_UIApp.swift
+//  I AM RICH Swift UI
+//
+//  Created by 효우 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct I_AM_RICH_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

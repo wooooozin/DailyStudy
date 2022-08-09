01c1b229af40365fa1bad802894e4332f9529224
wooooozin <wooooozin@gmail.com>
2022-08-09 19:43:57 +0900

    [feat] 프로젝트 생성

---
+++ LoopyBusinesscard/LoopyBusinesscard/LoopyBusinesscardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopyBusinesscardApp.swift
+//  LoopyBusinesscard
+//
+//  Created by 효우 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct LoopyBusinesscardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

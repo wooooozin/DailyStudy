e40eccb652995943df99288a1b7a10f587351f61
wooooozin <wooooozin@gmail.com>
2022-08-10 15:11:39 +0900

    [feat]프로젝트 생성

---
+++ HackerNewsSwiftUI/HackerNewsSwiftUI/HackerNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSwiftUIApp.swift
+//  HackerNewsSwiftUI
+//
+//  Created by 효우 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

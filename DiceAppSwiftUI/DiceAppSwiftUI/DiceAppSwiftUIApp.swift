558c13e8e9f0c80039a4f9f9c5e4ce00bc523291
wooooozin <wooooozin@gmail.com>
2022-08-10 12:57:01 +0900

    [feat] assets 추가

---
+++ DiceAppSwiftUI/DiceAppSwiftUI/DiceAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppSwiftUIApp.swift
+//  DiceAppSwiftUI
+//
+//  Created by 효우 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

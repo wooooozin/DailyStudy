f34783ac5ff2c01f41d2b57d7ea472f44cb84b45
wooooozin <wooooozin@gmail.com>
2022-08-10 13:53:53 +0900

    [feat] 버튼 기능 추가, @State, 포르젝트 완료

--- DiceAppSwiftUI/DiceAppSwiftUI/ContentView.swift
+++ DiceAppSwiftUI/DiceAppSwiftUI/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -17,13 +20,14 @@ struct ContentView: View {
                 Image("diceeLogo")
                 Spacer()
                 HStack {
-                    DiceView(number: 1)
-                    DiceView(number: 2)
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
                 }
                 .padding(.horizontal)
                 Spacer()
                 Button {
-                    print("click")
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
                 } label: {
                     Text("Roll")
                         .font(.system(size: 50))
